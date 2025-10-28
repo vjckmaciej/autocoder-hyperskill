8db15b23cd33f94bbf21721779b8c89dc4b66299
vjckmaciej <macwojcik20@gmail.com>
2025-10-28 22:34:41 +0100

    Initial commit, stage 1 completed

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+    Provide the full url of the GitHub repository that contains the code for the task between the quotes below.
+    For example:
+    url = "https://github.com/your-username/your-repo"
+"""
+
+url = "https://github.com/your-username/AutoCoder"
